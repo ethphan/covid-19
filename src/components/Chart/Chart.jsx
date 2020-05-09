9986086b50db6f778cdff31e6ded7ff4c8a1e857
Ethan's Mac <ethanphanvn@gmail.com>
2020-05-08 21:46:41 -0400

    update logo

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -17,7 +17,7 @@ const Chart = ({data : {confirmed, recovered, deaths}, country}) => {
 
     const lineChart =  (
         dailyData.length ?
-        <Line
+        <Line 
             data={{
                 labels: dailyData.map(({date}) => date),
                 datasets: [{
